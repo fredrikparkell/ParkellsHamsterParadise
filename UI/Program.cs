515fa8c172ca1108c78d7632bf8192caf4bbecbd
fredrikparkell <fredrikparkell5@gmail.com>
2021-04-03 22:04:40 +0200

    UI changes
    RunMainUI and StartSimulation added, not done though

--- UI/Program.cs
+++ UI/Program.cs
@@ -6,7 +6,15 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Console.SetWindowSize(180, 50);
+                UserInterface.RunMainUI();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- UI/UserInterface/UserInterface.cs
+++ UI/UserInterface/UserInterface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HamsterParadise.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,28 @@ using System.Threading.Tasks;
 
 namespace UI
 {
-    class UserInterface
+    public class UserInterface
     {
+        private static int totalDaysToSim;
+        private static int ticksPerSecond;
+        private static UserInput userInput = new UserInput();
+        private static UserPrint userPrint = new UserPrint();
+        private static CareHouseSimulation careHouseSimulation;
+
+        public static void RunMainUI()
+        {
+            // Metod som visar nån meny-print
+            // Meny-val för typ: Starta ny simulation, Sök på information om specifik simulation, etc
+
+        }
+
+
+
+        public static void StartSimulation(int daysToSimInput, int tickPerSecInput)
+        {
+            totalDaysToSim = daysToSimInput; // metod som returnerar antalet dagar att simulera
+            ticksPerSecond = tickPerSecInput; // metod som returnerar antalet ticks per sekund
+            careHouseSimulation = new CareHouseSimulation(ticksPerSecond, totalDaysToSim);
+        }
     }
 }
