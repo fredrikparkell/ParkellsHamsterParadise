471c397c19878e096bb6a5d2bc535298193037ca
fredrikparkell <fredrikparkell5@gmail.com>
2021-04-05 19:52:33 +0200

    New Migrations
    My Nullify-stored procedures

---
+++ HamsterParadise.DataAccess/Migrations/20210404135605_CreateSPForUpdatingCageSize.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Text;
+
+namespace HamsterParadise.DataAccess.Migrations
+{
+    public partial class CreateSPForUpdatingCageSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            StringBuilder cagesStoredProcedure = new StringBuilder();
+
+            cagesStoredProcedure.Append(@"CREATE PROCEDURE NullCageCageSize" + Environment.NewLine);
+            cagesStoredProcedure.Append("AS" + Environment.NewLine);
+            cagesStoredProcedure.Append("BEGIN" + Environment.NewLine);
+            cagesStoredProcedure.Append(@"UPDATE [Cages] SET [CageSize] = 0" + Environment.NewLine);
+            cagesStoredProcedure.Append("END" + Environment.NewLine);
+
+            migrationBuilder.Sql(cagesStoredProcedure.ToString());
+
+            StringBuilder exerciseAreaStoredProcedure = new StringBuilder();
+
+            exerciseAreaStoredProcedure.Append(@"CREATE PROCEDURE NullExerciseAreaCageSize" + Environment.NewLine);
+            exerciseAreaStoredProcedure.Append("AS" + Environment.NewLine);
+            exerciseAreaStoredProcedure.Append("BEGIN" + Environment.NewLine);
+            exerciseAreaStoredProcedure.Append(@"UPDATE [ExerciseAreas] SET [CageSize] = 0" + Environment.NewLine);
+            exerciseAreaStoredProcedure.Append("END" + Environment.NewLine);
+
+            migrationBuilder.Sql(exerciseAreaStoredProcedure.ToString());
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ HamsterParadise.DataAccess/Migrations/20210404142709_CreateSPForNullSomeHamsterValues.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Text;
+
+namespace HamsterParadise.DataAccess.Migrations
+{
+    public partial class CreateSPForNullSomeHamsterValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            StringBuilder hamsterNullStoredProcedure = new StringBuilder();
+
+            hamsterNullStoredProcedure.Append(@"CREATE PROCEDURE NullHamsterForNewSimulation" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append("AS" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append("BEGIN" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append(@"UPDATE [Hamsters] SET [CageId] = null" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append(@"UPDATE [Hamsters] SET [ExerciseAreaId] = null" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append(@"UPDATE [Hamsters] SET [CheckedInTime] = null" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append(@"UPDATE [Hamsters] SET [LastExerciseTime] = null" + Environment.NewLine);
+            hamsterNullStoredProcedure.Append("END" + Environment.NewLine);
+
+            migrationBuilder.Sql(hamsterNullStoredProcedure.ToString());
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
