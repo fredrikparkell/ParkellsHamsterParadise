6acfcc46b3d7c009771b83601ba8ad017d7fd979
fredrikparkell <fredrikparkell5@gmail.com>
2021-04-07 23:30:12 +0200

    UI changes

--- UI/UserInterface/UserPrint.cs
+++ UI/UserInterface/UserPrint.cs
@@ -10,113 +10,86 @@ namespace UI
 {
     public class UserPrint
     {
-        private int dayCounter;
-        private int prevDayTicker;
         private (int Left, int Top) currentPosition;
+
+        #region PrintTickInfo-methods
         public async void PrintTickInfo(object sender, TickInfoEventArgs e)
         {        
-            // Kunna visa:
-            // -Datumet/tiden/ticket/dag
-            // -Vilka hamstrar i vilka burar + ExerciseArea
-            // -Alla ActivityLogs som reggats detta tick
-
             await Task.Run(() =>
             {
                 if (e.ElapsedTicks == 1) { Console.Clear(); }
-                //Console.Clear();
-
-                ClearConsole();
 
                 Console.CursorVisible = false;
-                int weekDisplayPosition = 25;
-                int timeDisplayPosition = 60;
+                ClearConsole();
+
+                int titleDisplayPosition = 58; // 58
+                int timeDisplayPosition = 54;
                 int cageDisplayPosition = 70;
-                string[] days = new string[7] { "[Thursday],", "[Friday],", "[Saturday],", "[Sunday]", "[Monday],", "[Tuesday],", "[Wednesday]," };
 
-                // skriva ut veckodags-visning
-                Console.SetCursorPosition(weekDisplayPosition, 1);
-                for (int i = 0; i < days.Length; i++)
-                {
-                    if (i == dayCounter-1)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.Write($"{days[i]} ");
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        Console.Write($"{days[i]} ");
-                    }
-                }
+                Console.SetCursorPosition(titleDisplayPosition, 1);
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write($"HAMSTER PARADISE");
+                Console.ForegroundColor = ConsoleColor.White;
 
-
-
-                // skriva ut Tick
-                //currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition, currentPosition.Top + 1); Console.Write($"Ticks: {e.ElapsedTicks}");
-                // skriva ut Dag
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition, currentPosition.Top + 1); Console.Write(" ");
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition+8, currentPosition.Top + 1); Console.Write($"Day: {e.ElapsedDays}");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.SetCursorPosition(3, currentPosition.Top + 1); Console.Write("(Press Enter to Pause/Resume the simulation)");
+                Console.SetCursorPosition(83, currentPosition.Top + 1); Console.Write("(Press Enter to Pause/Resume the simulation)");
+                Console.ResetColor();
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition, currentPosition.Top + 1); Console.Write(" ");
-                // skriva ut Klockslag/datum
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition, currentPosition.Top + 1); Console.Write($"Date: {e.CurrentSimulationDate}");
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition, currentPosition.Top + 1); Console.Write(" ");
 
-
-
-                // skriva ut med String.Format     ACTIVITY          HAMSTER OVERVIEW
-                string header = String.Format("{0,-15} {1, 65}", "ACTIVITIES", "HAMSTER OVERVIEW");
-                currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(15, currentPosition.Top + 1);
+                string header = String.Format("{0,-15} {1, 70}", "ACTIVITIES", "HAMSTER OVERVIEW");
+                currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(25, currentPosition.Top + 1);
                 Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(header); Console.ResetColor();
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(1, currentPosition.Top + 1);
 
-
-
-                // skriva ut ----------------------------
-                Console.Write("-------------------------------------------------------------------------------------------------------------------------------------------------------");
-                // 
+                Console.Write("----------------------------------------------------------------------------------------------" +
+                    "---------------------------------------------------");
+                currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(1, currentPosition.Top + 1);
+                Console.Write(" ");
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(1, currentPosition.Top + 1);
                 Console.Write(" ");
 
-
-
-                //for (int i = 0; i < 30; i++)
-                //{
-                //    Console.SetCursorPosition(30, 8+i+1);
-                //    string something = String.Format("{0, 20}", "|");
-                //    Console.Write(something);
-                //}
-
-
                 (int Left, int Top) tempCurrentPosition = Console.GetCursorPosition();
-                // skriva ut alla ActivityLogs:      activity    |
-                for (int i = 0; i < e.ActivityLogs.Count(); i++)
+                int count = 0;
+                int rowCount = 0;
+                if (e.ActivityLogs.Count() != 0)
                 {
-                    currentPosition.Left = tempCurrentPosition.Left + 1;
-                    Console.SetCursorPosition(currentPosition.Left, currentPosition.Top + (i+1));
-                    string something = String.Format("{0, -30} {1, 20}", $"{e.ActivityLogs[i].Hamster.Name} => " +
-                                                              $"{e.ActivityLogs[i].Activity.ActivityName}", "|");
-                    Console.Write(something);
+                    while (true)
+                    {
+                        currentPosition.Left = tempCurrentPosition.Left + 1;
+                        Console.SetCursorPosition(currentPosition.Left, currentPosition.Top + (rowCount + 1));
+                        string something = String.Format("{0, -35} {1, -20}", $"{e.ActivityLogs[count].Hamster.Name} " +
+                                                                        $"=> {e.ActivityLogs[count].Activity.ActivityName}",
+                                                                       $"{e.ActivityLogs[count + 1].Hamster.Name}" +
+                                                                       $" => {e.ActivityLogs[count + 1].Activity.ActivityName}");
+                        Console.Write(something);
+                        count += 2;
+
+                        rowCount++;
+                        if (count > e.ActivityLogs.Count() - 1)
+                        {
+                            break;
+                        }
+                    
+                    }
                 }
 
-
-
-
                 Console.SetCursorPosition(cageDisplayPosition, tempCurrentPosition.Top);
-                WriteOutCage(e.HamstersInCages, 0, 5, cageDisplayPosition, tempCurrentPosition.Top);
+                WriteOutCage(e.HamstersInCages, 0, 5, cageDisplayPosition, tempCurrentPosition.Top-1);
                 currentPosition = Console.GetCursorPosition(); Console.SetCursorPosition(timeDisplayPosition, currentPosition.Top + 1); Console.Write(" ");
                 WriteOutCage(e.HamstersInCages, 5, 10, cageDisplayPosition, currentPosition.Top + 2);
 
-                // skriva ut ----------------------------
-                // 
-                //                          ExerciseArea
-                //
-                //                            Hamster
-                //                            Hamster
-                //                            Hamster
-                //                            osv
+                WriteOutExerciseArea(e.HamstersInExerciseArea);
+
+                DrawVerticalLine(65);
+                DrawVerticalLine(145);
             });
         }
-        public static void ClearConsole()
+        private void ClearConsole()
         {
             for (int i = 0; i < 40; i++)
             {
@@ -124,24 +97,77 @@ namespace UI
                 Console.Write(new string(' ', Console.WindowWidth));
             }
         }
+        private void DrawVerticalLine(int leftPosition)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.SetCursorPosition(leftPosition, 8 + i + 1);
+                string something = String.Format("{0, 0}", "|");
+                Console.Write(something);
+            }
+        }
+        private void WriteOutExerciseArea(List<HamsterParadise.DataAccess.Hamster> hamstersInExerciseArea)
+        {
+            Console.SetCursorPosition(98, 25);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Exercise Area");
+            Console.SetCursorPosition(98, 26);
+            Console.Write(" ");
+            Console.ResetColor();
+
+            for (int i = 0; i < hamstersInExerciseArea.Count(); i++)
+            {
+                Console.SetCursorPosition(101, 27+i);
+                Console.Write(hamstersInExerciseArea[i].Name);
+            }
+        }
         private void WriteOutCage(List<IGrouping<int?, HamsterParadise.DataAccess.Hamster>> hamsters, int startValue, int endValue,
                                                                 int cageDisplayPosition, int tempCurrentPositionTop)
         {
             for (int i = startValue; i < endValue; i++)
             {
-                Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 1);
-                Console.Write($"Cage {i + 1} ");
-                Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 2);
-                Console.Write(" ");
+                var cage = hamsters.Where(c => c.Key == i + 1).Select(h => h).FirstOrDefault();
+                if (cage != null)
+                {
+                    if (!cage.ElementAt(0).IsFemale)
+                    {
+                        Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 1);
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write($"Cage {i + 1} ");
+                        Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 2);
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 1);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"Cage {i + 1} ");
+                        Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 2);
+                        Console.Write(" ");
+                    }
+                }
+                else
+                {
+                    Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 1);
+                    Console.Write($"Cage {i + 1} ");
+                    Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 2);
+                    Console.Write(" ");
+                }
+                Console.ResetColor();
+
+                //Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 1);
+                //Console.Write($"Cage {i + 1} ");
+                //Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + 2);
+                //Console.Write(" ");
 
                 if (hamsters.Contains(hamsters.Select(h => h).AsEnumerable().Where(c => c.Key == i + 1).FirstOrDefault()))
                 {
-                    var cage = hamsters.Where(c => c.Key == i + 1).Select(h => h).First();
+                    //var cage = hamsters.Where(c => c.Key == i + 1).Select(h => h).First();
 
                     for (int j = 0; j < cage.Count(); j++)
                     {
                         Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + (2 + j + 1));
-                        Console.Write($"{cage.ElementAt(j).Name}     ");
+                        Console.Write($"{cage.ElementAt(j).Name}");
                     }
 
                     if (cage.Count() != 3)
@@ -150,7 +176,7 @@ namespace UI
                         {
                             currentPosition = Console.GetCursorPosition();
                             Console.SetCursorPosition(cageDisplayPosition, currentPosition.Top + (1 + x + 1));
-                            Console.Write("          ");
+                            Console.Write("");
                         }
                     }
                 }
@@ -159,14 +185,16 @@ namespace UI
                     for (int m = 0; m < 3; m++)
                     {
                         Console.SetCursorPosition(cageDisplayPosition, tempCurrentPositionTop + (2 + m + 1));
-                        Console.Write("          ");
+                        Console.Write("");
                     }
                 }
 
                 cageDisplayPosition += 15;
             }
         }
+        #endregion
 
+        #region PrintDayInfo-methods
         public async void PrintDayInfo(object sender, DayInfoEventArgs e)
         {
             // Kunna visa:
@@ -186,6 +214,9 @@ namespace UI
 
             });
         }
+        #endregion
+
+        #region PrintSimulationSummary
         public async void PrintSimulationSummary(object sender, SimulationSummaryEventArgs e)
         {
             await Task.Run(() =>
@@ -196,5 +227,6 @@ namespace UI
 
             });
         }
+        #endregion
     }
 }
