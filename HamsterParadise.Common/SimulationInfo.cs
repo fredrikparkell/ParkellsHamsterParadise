9c168dd8e287e0e8ce8db3b53ee51284f28cda94
fredrikparkell <fredrikparkell5@gmail.com>
2021-04-08 17:48:04 +0200

    Added SimulationInfo.cs
    To be able to pull out information about specific simulations

---
+++ HamsterParadise.Common/SimulationInfo.cs
@@ -0,0 +1,72 @@
+﻿using HamsterParadise.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterParadise.Common
+{
+    public class SimulationInfo
+    {
+        public List<string> GetAllSimulations()
+        {
+            List<string> allSimulations = new List<string>();
+
+            using (HamsterDbContext hamsterDb = new HamsterDbContext())
+            {
+                allSimulations = hamsterDb.Simulations.Where(s => s.ActivityLogs.Count() > 0)
+                                                      .OrderByDescending(s => s.Id)
+                                                      .Select(s => s.Name).ToList();
+            }
+
+            return allSimulations;
+        }
+        public List<string> GetAllDays(int simulationId)
+        {
+            List<string> allDays = new List<string>();
+
+            using (HamsterDbContext hamsterDb = new HamsterDbContext())
+            {
+                var allDaysIGrouping = hamsterDb.ActivityLogs.Where(s => s.SimulationId == simulationId)
+                                                .AsEnumerable().GroupBy(t => t.TimeStamp.Day).ToList();
+                allDays = allDaysIGrouping.Select(t => t.Key.ToString()).ToList();
+            }
+
+            return allDays;
+        }
+        public DayInfoEventArgs GetAllActivityLogsPerDay(int day, int simulationId)
+        {
+            List<IGrouping<int, ActivityLog>> activityLogsPerHamster;
+            List<Hamster> hamsters = new List<Hamster>();
+            DateTime currentDate = new DateTime();
+
+            using (HamsterDbContext hamsterDb = new HamsterDbContext())
+            {
+                activityLogsPerHamster = hamsterDb.ActivityLogs.Where(a => a.TimeStamp.Day == day
+                                                                 && a.SimulationId == simulationId).AsEnumerable().GroupBy(h => h.HamsterId)
+                                                                 .OrderBy(h => h.Key).ToList();
+                hamsters = hamsterDb.Hamsters.ToList();
+
+                currentDate = hamsterDb.ActivityLogs.Where(a => a.TimeStamp.Day == day && a.SimulationId == simulationId)
+                                                    .Select(t => t.TimeStamp).First();
+            }
+
+            return new DayInfoEventArgs(0, day, simulationId, currentDate, activityLogsPerHamster, hamsters);
+        }
+        public SimulationSummaryEventArgs GetAllActivityLogsPerSimulation(int totalDays, int simulationId)
+        {
+            List<IGrouping<int, ActivityLog>> activityLogsPerHamster;
+            List<Hamster> hamsters = new List<Hamster>();
+
+            using (HamsterDbContext hamsterDb = new HamsterDbContext())
+            {
+                activityLogsPerHamster = hamsterDb.ActivityLogs.Where(a => a.SimulationId == simulationId).AsEnumerable().GroupBy(h => h.HamsterId)
+                                                                 .OrderBy(h => h.Key).ToList();
+                hamsters = hamsterDb.Hamsters.ToList();
+            }
+
+            return new SimulationSummaryEventArgs(0,totalDays,simulationId,DateTime.Now,activityLogsPerHamster, hamsters);
+        }
+    }
+}
