756be8b24fa03a80275fab006c5445d47101a905
fredrikparkell <fredrikparkell5@gmail.com>
2021-04-09 16:54:15 +0200

    Added a Linq-extension to shuffle an IEnumerable<T>

---
+++ HamsterParadise.Common/LinqExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterParadise.Common
+{
+    public static class LinqExtensions
+    {
+        private static Random random = new Random();
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> data)
+        {
+            if (data.Count() == 0)
+            {
+                return null;
+            }
+
+            var newData = data.ToList();
+
+            int newDataCount = newData.Count;
+            while (newDataCount > 1)
+            {
+                newDataCount--;
+                int randomValue = random.Next(newDataCount + 1);
+                T value = newData[randomValue];
+                newData[randomValue] = newData[newDataCount];
+                newData[newDataCount] = value;
+            }
+
+            return (IEnumerable<T>)newData;
+        }
+    }
+}
