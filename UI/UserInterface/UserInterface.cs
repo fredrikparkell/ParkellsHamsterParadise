c4d109616b53c9da108defb479971e5ca19f418c
fredrikparkell <fredrikparkell5@gmail.com>
2021-04-10 03:14:03 +0200

    Added simulation-note

--- UI/UserInterface/UserInterface.cs
+++ UI/UserInterface/UserInterface.cs
@@ -146,6 +146,10 @@ namespace UI
             while (true)
             {
                 Console.Clear();
+                Console.SetCursorPosition(1, 22); Console.ForegroundColor = ConsoleColor.DarkRed; Console.Write("NOTE: 5, 6 & 7 ticks per second has been tested a total of 50+ times (15-20+ times each) with a range of 2-8 days runtime.");
+                Console.SetCursorPosition(1, 23); Console.Write("In only one, a 7 tick per second (142 ms/tick) 8 day simulation, did it crash mid-simulation. This due to a update going a little too slow."); 
+                Console.SetCursorPosition(1, 24); Console.Write("It ran fine on 7 tick simulations directly after that aswell. Thus, there should not be any issues running the simulation on up to 7 ticks per second."); Console.ResetColor();
+
                 string title = @"
   _______ _      _                                                         _ ___  
  |__   __(_)    | |                                                       | |__ \ 
@@ -157,7 +161,7 @@ namespace UI
                            |_|                                                    
 (Use the arrow keys to cycle through options and press enter to select an option.)";
                 string[] options = new string[] { "  Default (2)  ", "      (1)      ", "      (2)      ",
-                                                "      (3)      ", "      (4)      ", "      (5)      ",
+                                                "      (3)      ", "Recommended (4)", "      (5)      ",
                                                 "      (6)      ", "      (7)      " };
 
                 UserMenu ticksPerSecondMenu = new UserMenu(title, options, 10, 25);
